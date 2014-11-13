c4ed3d8c776daea3ce25ea15bc6779e49033906c
goen <fuuucl.am@gmail.com>
2014-11-13 02:36:04 +0100

    This is the final state of the algorithm

--- unrank.go
+++ unrank.go
@@ -5,73 +5,60 @@ import (
 )
 
 var (
-	ErrCmp = errors.New("Unrank: comparation failed")
-	ErrNul = errors.New("Unrank: zeroes underrun")
-	ErrOne = errors.New("Unrank: ones underrun")
+	ErrUnr = errors.New("Unrank: Treshold error")
+	ErrNul = errors.New("Unrank: Zeroes underrun")
+	ErrOne = errors.New("Unrank: Ones underrun")
 )
 
-const (
-	debug = 1
-)
-
-// Binomialer provides tresholders for the x,y field from the pascal's triangle
-// If the x or y == 0 it gives the number 0
-// If the x or y == 1 it gives 1
-// If the x or y == 2 it gives y or x, etc..
-type Binomialer interface {
-	Binomial(uint, uint) Tresholder
-}
-
-// Tresholder is typically an unsigned number
-// The Cmp() compares itself to arg and returns -1 iff reciever < arg
-// Cmp() may return -2 if the comparation failed
-// The Sub() makes reciever smaller by argument
+// CmpSub compares the treshold to the binomial number with parameters xx, yy.
+// If the treshold is larger or equal, number is substracted from treshold.
+// Returns -1 iff treshold < binomial (not-substracted)
+// Returns -2 iff an error occured (not substracted)
+// Else it substracts and returns the binomial coefficient in any format.
 type Tresholder interface {
-	Cmp(Tresholder) int
-	Sub(Tresholder)
+	CmpSub(xx, yy uint) (int, interface{})
+	Sub(interface{})
 }
 
-// Type set represents the result bit set. The SetBit set's 
+// Set represents the actual underlying result bit set.
+// The n-th bit is set to b == {0,1}
 type Set interface {
-	SetBit(int, b uint)
+	SetBit(n uint, b uint)
 }
 
 // Unrank implements the combination unrank algorithm
-func Unrank(s Set, b Binomialer, tr Tresholder, z1 uint, z0 uint) error {
-	var n, b, swap uint
+// The unrank treshold is provided as part of the Tresholder argument
+func Unrank(s Set, t Tresholder, z1 uint, z0 uint) error {
+	var n, bit, swap uint
 
 	for z0 + z1 > 0 {
-		// avoid substracting
+		// a cool swap to minimize subtractions
 		if z1 > z0 {
-			b = 1-b
+			bit = 1-bit
 			swap = z0
 			z0 = z1
 			z1 = swap
 		}
 
-		bi := b.Binomial(z0, z1 + 1)
-		cmp := tr.Cmp(bi)
+		cmp, _ := t.Treshold(z0, z1 + 1)
 
 		switch cmp {
+		case -2:
+			return ErrUnr
 		case -1:
-			s.SetBit(n, b)
+			s.SetBit(n, bit)
 
 			if z0 == 0 {
 				return ErrNul
 			}
 			z0--
-		case 0:
-			fallthrough
-		case 1:
-			s.SetBit(n, 1-b)
-			tr.Sub(bi)
+		default:
+			s.SetBit(n, 1-bit)
 
 			if z1 == 0 {
 				return ErrOne
 			}
 			z1--
-		default:
-			return ErrCmp
 		}
 
 		n++;
